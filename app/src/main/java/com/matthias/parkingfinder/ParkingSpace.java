4ad6a90a5ee182c12d79c96e3a2483ce565d8103
Matthias <iMatthias@users.noreply.github.com>
2016-02-27 20:22:44 -0600

    Type Checking for Parking Space

--- app/src/main/java/com/matthias/parkingfinder/ParkingSpace.java
+++ app/src/main/java/com/matthias/parkingfinder/ParkingSpace.java
@@ -1,18 +1,29 @@
 package com.matthias.parkingfinder;
 
+import java.util.ArrayList;
+
 /**
  * Created by Matthias on 16-02-27.
  */
 public class ParkingSpace
 {
+	// constant var for parking space type
+	private final static int STREET_PARKING = 0;
+	private final static int PARKADE        = 1;
+	private final static int SURFACE_LOT    = 2;
+
+	// details of the parking space
 	private double cost;
 	private Time chargingTime;          // non-charging time = 24 hours - charging time - nonParkingTime
 	private Time nonParkingTime;        // times when people are not allowed to park
 	private Location location;
 	private boolean hasCamera;
 	private boolean hasAttendant;
+	private int type;
 
-	public ParkingSpace(double cost, Time chargingTime, Time nonParkingTime, Location location, boolean hasCamera, boolean hasAttendant)
+
+	public ParkingSpace(double cost, Time chargingTime, Time nonParkingTime, Location location,
+	                    boolean hasCamera, boolean hasAttendant, int type)
 	{
 		this.cost = cost;
 		this.chargingTime = chargingTime;
@@ -20,7 +31,37 @@ public class ParkingSpace
 		this.location = location;
 		this.hasCamera = hasCamera;
 		this.hasAttendant = hasAttendant;
+		this.type = type;
 	}
 
+	// ================================================================
+	// Methods to check parking Type
+	// ================================================================
 
+	boolean isStreetParking()
+	{ return type == STREET_PARKING; }
+
+	boolean isSurfaceLot()
+	{ return type == SURFACE_LOT; }
+
+	boolean isParkade()
+	{ return type == PARKADE; }
+
+
+	// ================================================================
+	// Methods for stub DB
+	// ================================================================
+
+	static ArrayList<ParkingSpace> getStubList()
+	{
+		ParkingSpace A = null;
+		ParkingSpace B = null;
+		// ...
+
+		ArrayList<ParkingSpace> list = new ArrayList<>();
+		list.add(A);
+		list.add(B);
+
+		return list;
+	}
 }
