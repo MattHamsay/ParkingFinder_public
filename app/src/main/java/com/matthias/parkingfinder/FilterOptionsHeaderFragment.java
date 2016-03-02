6c1bda45c89d5445feb40932802debb7c0b2d422
Harman <harman.brar@outlook.com>
2016-03-01 21:32:27 -0600

    Fixed Crashing

--- app/src/main/java/com/matthias/parkingfinder/FilterOptionsHeaderFragment.java
+++ app/src/main/java/com/matthias/parkingfinder/FilterOptionsHeaderFragment.java
@@ -1,29 +1,21 @@
 package com.matthias.parkingfinder;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
-public class FilterOptionsHeaderFragment extends AppCompatActivity {
 
+/**
+ * Created by harma on 3/1/2016.
+ */
+public class FilterOptionsHeaderFragment extends Fragment
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_filter_options_header_fragment);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_filter_options, container, false);
+        return view;
     }
-
 }
